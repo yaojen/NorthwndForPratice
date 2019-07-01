03e5ccc2a5cac39dcc20fb6c72f5a16a4c8240ae
yaojen <yaojenliang@gmail.com>
2019-07-01 11:45:56 +0800

    加入專案檔案。

---
+++ Northwnd.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Northwnd.Web.Startup))]
+namespace Northwnd.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
