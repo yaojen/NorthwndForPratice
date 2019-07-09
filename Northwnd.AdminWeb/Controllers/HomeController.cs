2ad43f97fba8d06cb03bfcdc8a26ae4bdc06ffab
yaojen <yaojenliang@gmail.com>
2019-07-09 12:35:24 +0800

    add login function

--- Northwnd.AdminWeb/Controllers/HomeController.cs
+++ Northwnd.AdminWeb/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using Northwnd.AdminWeb.ViewModels;
 
 namespace Northwnd.AdminWeb.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +29,49 @@ namespace Northwnd.AdminWeb.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel loginViewModel)
+        {
+            if (loginViewModel.Id == "kevin" && loginViewModel.Password == "123456")
+            {
+                var now = DateTime.Now;
+                var roles = GetRole();
+                var ticket = new FormsAuthenticationTicket(
+                            version: 1,
+                            name: loginViewModel.Id,
+                            issueDate: now,
+                            expiration: now.AddMinutes(30),
+                            isPersistent: false,
+                            userData: roles,
+                            cookiePath: FormsAuthentication.FormsCookiePath);
+                var encryptedTicket = FormsAuthentication.Encrypt(ticket);
+                var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+                Response.Cookies.Add(cookie);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        private string GetRole()
+        {
+            return "Admin,User,HR";
         }
     }
 }
